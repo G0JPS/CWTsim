8c6986958c98f87e3cff9bdcb1bb6a774c63880e
G0JPS <47941689+G0JPS@users.noreply.github.com>
2019-10-08 21:37:28 +0100

    Update CWTsim.pl
    Now reads directly from the member roster as the previous web member list is currently not being updated.

--- CWTsim.pl
+++ CWTsim.pl
@@ -1,53 +1,57 @@
 #!/usr/bin/perl
 
-# CWTsim
-# G0JPS [2080], Jun 2019
+# CWOPspotter
+# G0JPS [2080], Feb 2019
 
-# WHAT IS CWTsim?
+# WHAT IS CWOPCallsigns?
 # The script does two things. It fetches a CWOPs member list from
-# the web and stores it in memory.
+# the web and stores it in an array.
 #
-# Then it displays a selction of random entries for an advisor
+# Secondly it displays a selction of random entries for an advisor
 # to send to students during a zoom session for CWT practice.
 #
+# *If* I can get sound working, I hope to make it simulate a running station!
+
 
 # SUPPORT
 # 1. RTFM
-# 2. Ah. There is no manual.
-# 3. Read XKCD instead. It won't help with this program, but it's funny.
+# 2. There is no manual.
 
 # CONFIGURATION - IMPORTANT
-# How many callsign/name/number to show?
+
+# How many callsigns to display / work?
 my $runs = 20;
 
 use strict;
 use warnings;
+
 my $callsign = "";
 
 my %cwops;
+my %cwopsheard;
 my $random;
 my $mems;
+my $ents = 0;
+my $runner;
 
 print "\033[2J";
 print "\033[0;0H";
-print "CWTsim : started\n";
+print "CWOpCallsigns : started\n";
 
 #Get membership list from the web
 sub getmembers{
 	print "Getting member list...";
-	my $content = qx{curl http://hamclubs.info/lists/CWOPS_members.txt};
+	my $content = qx{curl https://docs.google.com/spreadsheets/d/1Ew8b1WAorFRCixGRsr031atxmS0SsycvmOczS_fDqzc/export?format=csv};
 	die "CWOPS: No Web Data\n" unless defined $content;
 
 	while ($content =~ /([^\n]+)\n?/g){
-		my $line = uc($1);
+		my $line = $1;
 		chomp $line;
-		my ($exp, $call, $number, $name, $d1) = split / /, $line;
+		my ($null,$exp, $call, $number, $name, $d1) = split /,/, $line;
 		$cwops{$call}=$call . " " . $name . " " . $number;
 	}
 $mems = (keys %cwops);
-print "\033[2J";
-print "\033[0;0H";
-print "$mems callsigns downloaded. ";
+print "Done - $mems members downloaded.\n\n";
 }
 
 sub getacallsign{
@@ -63,9 +67,14 @@ if ($runs >= $mems){
 	die "More runs than callsigns!";
 }
 
+# Set the running station
+getacallsign();
+$runner = $callsign;
+print "Running Station is $runner\n\n";
+sleep 1;
 
 # Display some callsigns
-print "Displaying $runs callsigns...\n\n";
+print "Displaying some callsigns...\n\n";
 
 while ($runs >= 0){
 	getacallsign();
@@ -74,5 +83,5 @@ while ($runs >= 0){
 	sleep 1;
 }	
 
-print "\n73!\n";
+print "\n\n73!\n";
 
